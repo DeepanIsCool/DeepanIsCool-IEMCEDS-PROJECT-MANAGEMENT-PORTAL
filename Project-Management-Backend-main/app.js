1c4ba9c559e65265b00d261e4a9c70feb6634d6b
DeepanIsCool <deepan.sadhukhan2023@iem.edu.in>
2024-08-22 23:54:12 +0530

    chore: Update allowed origins in app.js

--- Project-Management-Backend-main/app.js
+++ Project-Management-Backend-main/app.js
@@ -17,12 +17,13 @@ connectDb();
 
 // Middleware
 app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174"
+const allowedOrigins = "*";
+// [
+//   "http://localhost:5173",
+//   "http://localhost:5174"
   
   
-];
+// ];
 
 
 const corsOptions = {
