69c51ce03898a713933835ff6478e3bc451d9b94
DeepanIsCool <deepan.sadhukhan2023@iem.edu.in>
2024-08-23 00:11:50 +0530

    refactor: Update app.js to use '/api' prefix for routes

--- Project-Management-Backend-main/app.js
+++ Project-Management-Backend-main/app.js
@@ -1,11 +1,8 @@
-//app,.js
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDb = require('./config/db'); // Database connection
-const studentsRoutes = require('./Students/studentsRoutes');
 const cors = require("cors");
 require("dotenv").config();
-const session = require("express-session");
 const fs = require('fs');
 const path = require('path');
 
@@ -17,26 +14,10 @@ connectDb();
 
 // Middleware
 app.use(express.json());
-const allowedOrigins = "*";
-// [
-//   "http://localhost:5173",
-//   "http://localhost:5174"
-  
-  
-// ];
 
+// Use CORS middleware to allow all origins
+app.use(cors());
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }  
-  },
-};
-
-app.use(cors(corsOptions));
 const resumeDir = path.join(__dirname, 'Resumes');
 
 // Check if the "resume" directory exists, if not, create it
@@ -46,17 +27,17 @@ if (!fs.existsSync(resumeDir)) {
 } else {
   console.log('Directory "resume" already exists.');
 }
+
 // Routes
-// app.use('/studentsRoutes', studentsRoutes);
 app.use('/api/studentsRoutes', require("./Students/studentsRoutes"));
 app.use('/api/facultyRoutes', require("./Faculty/facultyRoutes"));
-//app.use('/adminRoutes', require("./Admin/adminRoutes"));
 app.use('/api/projectRoutes', require("./Projects/projectRoutes"));
 
 app.get("/api", (req, res) => {
     res.send("Welcome to the Project Management Backend");
-  });
+});
+
 // Start server
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
